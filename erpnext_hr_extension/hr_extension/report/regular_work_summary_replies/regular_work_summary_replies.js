7da6c80e6b1a6039deb820456f133ea8675eac24
Aleksas Pielikis <ant.kampo@gmail.com>
2019-05-16 14:34:55 +0300

    Add report

---
+++ erpnext_hr_extension/hr_extension/report/regular_work_summary_replies/regular_work_summary_replies.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Aleksas Pielikis and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Regular Work Summary Replies"] = {
+	"filters": [
+
+	]
+}
